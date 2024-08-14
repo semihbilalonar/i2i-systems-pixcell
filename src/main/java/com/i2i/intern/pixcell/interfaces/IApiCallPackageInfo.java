31b1647c67f98d29efa664c3b2e9db4aa28c507d
Ayşe Elif Çelik <147512758+ayelfce@users.noreply.github.com>
2024-08-14 10:42:37 +0300

    android first commit

---
+++ src/main/java/com/i2i/intern/pixcell/interfaces/IApiCallPackageInfo.java
@@ -0,0 +1,11 @@
+package com.i2i.intern.pixcell.interfaces;
+
+import com.i2i.intern.pixcell.data_model.PackageInfoResponse;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+
+public interface IApiCallPackageInfo {
+    // Code blocks
+}
---
+++ src/main/java/com/i2i/intern/pixcell/interfaces/IApiCallSignUp.java
@@ -0,0 +1,14 @@
+package com.i2i.intern.pixcell.interfaces;
+
+import com.i2i.intern.pixcell.data_model.SignUpResponse;
+
+import java.util.Map;
+
+import retrofit2.Call;
+import retrofit2.http.FieldMap;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.POST;
+
+public interface IApiCallSignUp {
+    // Code blocks
+}
